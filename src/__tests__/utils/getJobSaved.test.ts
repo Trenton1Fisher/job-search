ba175c4904fa3624af5e95b74aaf0443812af1dd
Trenton1Fisher <trenton0fisher@gmail.com>
2025-03-25 14:53:02 -0700

    still doing some basic tests

--- src/__tests__/utils/getJobSaved.test.ts
+++ src/__tests__/utils/getJobSaved.test.ts
@@ -6,7 +6,7 @@ test('(User_id undefined) Get saved Job should return false', async () => {
   expect(result).toBe(false)
 })
 
-test('(Job_id undefined) Get saved Job should return false', async () => {
+test('(Job_id not in range) Get saved Job should return false', async () => {
   const result = await getIsJobSaved('user_id', -1)
   expect(result).toBe(false)
 })
