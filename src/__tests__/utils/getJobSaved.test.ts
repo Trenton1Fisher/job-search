0c411d00558d2f865fd76c5e2a32211d73103b50
Trenton1Fisher <trenton0fisher@gmail.com>
2025-03-25 15:38:04 -0700

    vercel pg gotta go

--- src/__tests__/utils/getJobSaved.test.ts
+++ src/__tests__/utils/getJobSaved.test.ts
@@ -1,12 +1,16 @@
 import { getIsJobSaved } from '@/utils/getJobSaved'
-import { expect, jest, test } from '@jest/globals'
+import { describe, jest, expect, test, beforeAll } from '@jest/globals'
+import { createPool, sql } from '@vercel/postgres'
+import { beforeEach } from 'node:test'
 
-test('(User_id undefined) Get saved Job should return false', async () => {
-  const result = await getIsJobSaved('', 0)
-  expect(result).toBe(false)
-})
+describe('getIsJobSaved helper function', () => {
+  test('(User_id undefined) Get saved Job should return false', async () => {
+    const result = await getIsJobSaved('', 0)
+    expect(result).toBe(false)
+  })
 
-test('(Job_id not in range) Get saved Job should return false', async () => {
-  const result = await getIsJobSaved('user_id', -1)
-  expect(result).toBe(false)
+  test('(Job_id not in range) Get saved Job should return false', async () => {
+    const result = await getIsJobSaved('user_id', -1)
+    expect(result).toBe(false)
+  })
 })
