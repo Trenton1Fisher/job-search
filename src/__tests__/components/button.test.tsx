13e59f6cad892c1d9d28d197822f0dc736cf43bc
Trenton1Fisher <trenton0fisher@gmail.com>
2025-03-28 17:34:37 -0700

    button testing complete

---
+++ src/__tests__/components/button.test.tsx
@@ -0,0 +1,30 @@
+import Button from '@/components/ui/button'
+import { render, screen } from '@testing-library/react'
+import { useFormStatus } from 'react-dom'
+
+jest.mock('react-dom', () => ({
+  ...jest.requireActual('react-dom'),
+  useFormStatus: jest.fn(),
+}))
+
+describe('Button Ui Component', () => {
+  const renderButton = (pending: boolean) => {
+    const MockButtonStatus = useFormStatus as jest.Mock
+    MockButtonStatus.mockReturnValue({ pending })
+
+    render(<Button />)
+  }
+
+  it('should render button component', () => {
+    renderButton(false)
+    const buttonValue = screen.getByText('Search')
+    expect(buttonValue).toBeInTheDocument()
+    expect(buttonValue).not.toBeDisabled()
+  })
+
+  it('button should be disabled', () => {
+    renderButton(true)
+    const buttonValue = screen.getByText('Searching')
+    expect(buttonValue).toBeDisabled()
+  })
+})
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,10 +1,13 @@
-import { useFormStatus } from "react-dom";
+import { useFormStatus } from 'react-dom'
 
 export default function Button() {
-  const { pending } = useFormStatus();
+  const { pending } = useFormStatus()
   return (
-    <button className="bg-blue-500 text-white rounded-2xl font-bold uppercase p-3 transition-transform hover:scale-105 hover:bg-blue-700">
-      {pending ? "Searching" : "Search"}
+    <button
+      disabled={pending}
+      className="bg-blue-500 text-white rounded-2xl font-bold uppercase p-3 transition-transform hover:scale-105 hover:bg-blue-700"
+    >
+      {pending ? 'Searching' : 'Search'}
     </button>
-  );
+  )
 }
